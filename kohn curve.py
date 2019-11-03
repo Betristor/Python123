d5ca45dd1bdc6ed73639cd947910f642ab5a734e
Zhang Yuheng <2845024327@qq.com>
2019-11-03 19:39:22 +0800

    kohn curve

---
+++ kohn curve.py
@@ -0,0 +1,27 @@
+import turtle
+def kohn_curve(size,n):
+    if n == 0:
+        turtle.fd(size)
+    else:
+        for angle in [0,60,-120,60]:
+            turtle.left(angle)
+            kohn_curve(size/3,n-1)
+
+def main():
+    turtle.setup(600,600)
+    turtle.penup()
+    turtle.goto(-200,100)
+    turtle.pendown()
+    turtle.pensize(2)
+    turtle.begin_fill()
+    level = 4
+    kohn_curve(400,level)
+    turtle.right(120)
+    kohn_curve(400,level)
+    turtle.right(120)
+    kohn_curve(400,level)
+    turtle.end_fill()
+    turtle.hideturtle()
+    turtle.done()
+
+main()
