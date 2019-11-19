2d97c7d817bfdca2dcda4362b77189b674b184df
Zhang Yuheng <2845024327@qq.com>
2019-11-19 22:18:09 +0800

    libs changed again

--- auto_pip.py
+++ auto_pip.py
@@ -1,4 +1,4 @@
 import os
-libs = ["tensorflow","mxnet"]
+libs = ["pyspider","python-goose","pygoblect","panda3d","cocos2d","vr zero","vizard","quads","ascii_art"]
 for lib in libs:
     os.system("pip install " + lib)
